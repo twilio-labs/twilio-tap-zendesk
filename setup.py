90faeeb2e98bb9e615aea2e76b379b1d17c4970f
Edgar Ramírez Mondragón <16805946+edgarrmondragon@users.noreply.github.com>
2024-07-03 15:59:06 -0600

    Require Python >= 3.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
       install_requires=[
           'pipelinewise-singer-python==1.2.0',
           'zenpy==2.0.49',
-          'requests==2.31.0',
+          'requests==2.32.3',
       ],
       extras_require={
           'dev': [
@@ -27,6 +27,7 @@ setup(
           [console_scripts]
           tap-zendesk=tap_zendesk:main
       ''',
+      python_requires=">=3.8",
       packages=['tap_zendesk'],
       include_package_data=True,
 )
