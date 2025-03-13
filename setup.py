ef6231f2d95e8fda4603647bf3fe2b9447a7492b
Aishwarya Gupta <aigupta@twilio.com>
2025-03-13 16:40:23 +0000

    de-210

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
       install_requires=[
           'pipelinewise-singer-python==1.2.0',
           'zenpy==2.0.49',
-          'requests==2.20.0'
+          'requests==2.29.0'
       ],
       extras_require={
           'dev': [
